7faf2d2d532df6049cbcb9ec2091a5bae5d6dae6
metamellow <115264178+metamellow@users.noreply.github.com>
2024-06-20 04:10:16 +0700

    added env stuff

--- wp-config.php
+++ wp-config.php
@@ -1,62 +1,28 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
+
+require_once(__DIR__ . '/load-env.php');
+
+require_once(__DIR__ . '/vendor/autoload.php');
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'u834651083_coinquant');
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']);
 
-/** MySQL database username */
-define('DB_USER', 'u834651083_admin');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'Blockwarp2024');
-
-/** MySQL hostname */
-define('DB_HOST', 'aliceblue-mallard-545481.hostingersite.com');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'lq2A+E+{5`,>dWE;IIcxUxdL~)w%lI;*nOGw#r/,5ph.gchc%+Z|Yuz;I.7l#LQ~');
-define('SECURE_AUTH_KEY',  '[>CA3Na`.,|B@mbvQ,xvY&V>U<M*OGrx0E|)4o9U}Icl J(o17q@+-{=*}kicZ9L');
-define('LOGGED_IN_KEY',    '8B`<E#0vuiJaD/,J-y1BT)Xu$d3YUCgi%>UDSI>M`r|= 7h|LA4RItDoqGZ{Rn|I');
-define('NONCE_KEY',        'eS4R7J#XmfZl1X!~6+1cT+r3-|{=VJ~B./Xoqx@a1Gw5RijC0|6R8y[|NHCUiV+)');
-define('AUTH_SALT',        '`zP!3n=xG~O_]8Q`VQbiY3*bo}Ng; |(11F$w|2|=W:CL&Cc.:`T!+-TOwYp9 +}');
-define('SECURE_AUTH_SALT', 'PR&iDT8y7yPA;Jh.tX1w|CPeL7Hun.@u/CwG[YJfLDpL-]:+wap}d/V+8RIClyA!');
-define('LOGGED_IN_SALT',   '4L[Xrzw<>ZnCc)HE TO_2+%!VQ#:=yK2a!bMppGxmUchp<Iwu-3||j=-.852$J;B');
-define('NONCE_SALT',       '`P5~#AlR7S@~d>`mL&^@K+Z#+UI ~I@t}9iVpCc/IZCO]uL8v}|OlF)#<e(6r)dx');
-
-/**#@-*/
+// ** Authentication Unique Keys and Salts. ** //
+define('AUTH_KEY', $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['NONCE_KEY']);
+define('AUTH_SALT', $_ENV['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['NONCE_SALT']);
 
 /**
  * WordPress Database Table prefix.
