f3eb2c0e119fc84ea130fd45961983594252dafb
metamellow <115264178+metamellow@users.noreply.github.com>
2024-06-20 04:23:58 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,56 +1,29 @@
 <?php
-
 require_once(__DIR__ . '/load-env.php');
 
-require_once(__DIR__ . '/vendor/autoload.php');
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-
 // ** MySQL settings - You can get this info from your web host ** //
-define('DB_NAME', $_ENV['DB_NAME']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
 
 // ** Authentication Unique Keys and Salts. ** //
-define('AUTH_KEY', $_ENV['AUTH_KEY']);
-define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
-define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
-define('NONCE_KEY', $_ENV['NONCE_KEY']);
-define('AUTH_SALT', $_ENV['AUTH_SALT']);
-define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
-define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
-define('NONCE_SALT', $_ENV['NONCE_SALT']);
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+/** The rest of the wp-config.php file **/
+
 $table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
 define('WP_DEBUG', true);
 
-/* That's all, stop editing! Happy blogging. */
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__) . '/');
+}
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
